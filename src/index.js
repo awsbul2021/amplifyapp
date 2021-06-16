f20b94c6ad7a34a0770d48c739184366e8a9f58b
柏原佑太 <y-kashihara@kashiwabarayuufutoshi-no-MacBook-Pro.local>
2021-06-17 00:07:52 +0900

    test1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 
 ReactDOM.render(
   <React.StrictMode>
