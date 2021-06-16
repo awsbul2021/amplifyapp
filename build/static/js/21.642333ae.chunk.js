a7557a2192eafc74328b4c96505b5830dfa11b6f
柏原佑太 <y-kashihara@kashiwabarayuufutoshi-no-MacBook-Pro.local>
2021-06-17 07:01:11 +0900

    added build

---
+++ build/static/js/21.642333ae.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpamplifyapp=this.webpackJsonpamplifyapp||[]).push([[21],{372:function(t,e,n){"use strict";n.r(e),n.d(e,"amplify_container",(function(){return c}));var i=n(9),c=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("slot",null))},t}();c.style="amplify-container{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
+//# sourceMappingURL=21.642333ae.chunk.js.map
